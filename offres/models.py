76bade2febe4e226b9591efc73dd56351148351e
Beonel <jean.beonel@outlook.fr>
2024-10-21 00:30:16 +0200

    modif

--- offres/models.py
+++ offres/models.py
@@ -12,7 +12,7 @@ class Sport(models.Model):
 class TypeOffre(models.Model):
     nom = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    Prix = models.DecimalField(max_digits=10, decimal_places=2)
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nom
