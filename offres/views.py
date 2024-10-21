43826493b10e0a5a002ba11dedff213524bf473d
Beonel <jean.beonel@outlook.fr>
2024-10-21 02:05:53 +0200

    modif

--- offres/views.py
+++ offres/views.py
@@ -12,9 +12,6 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 
-import logging
-
-logger = logging.getLogger(__name___)
 
 
 #Vue pour l'inscription
